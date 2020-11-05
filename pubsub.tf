f606922234e978e4a075c58cb8fbf40a40fa0b45
supersmo <supersmo@something.com>
2020-11-05 16:48:17 +0100

    Add pub sub resources back

--- pubsub.tf
+++ pubsub.tf
@@ -1,25 +1,25 @@
-//### PUBSUB
-//resource "google_pubsub_topic" "example" {
-//  project = var.DEPLOY_PROJECT
-//  name    = "example-topic-${var.HACKATHON_TEAM_NAME}"
-//
-//  message_storage_policy {
-//    allowed_persistence_regions = [
-//      "europe-north1",
-//      "europe-west1",
-//      "europe-west4",
-//    ]
-//  }
-//
-//  labels = {
-//    ownerteam     = var.HACKATHON_TEAM_NAME
-//  }
-//}
-//
-//### GIVE SERVICE ACCOUNT ACCESS AS PUBLISHER
-//resource "google_pubsub_topic_iam_member" "example-sa-publisher" {
-//  project   = var.DEPLOY_PROJECT
-//  topic     = google_pubsub_topic.example.name
-//  role      = "roles/pubsub.publisher"
-//  member    = "serviceAccount:${google_service_account.example.email}"
-//}
+### PUBSUB
+resource "google_pubsub_topic" "example" {
+  project = var.DEPLOY_PROJECT
+  name    = "example-topic-${var.HACKATHON_TEAM_NAME}"
+
+  message_storage_policy {
+    allowed_persistence_regions = [
+      "europe-north1",
+      "europe-west1",
+      "europe-west4",
+    ]
+  }
+
+  labels = {
+    ownerteam     = var.HACKATHON_TEAM_NAME
+  }
+}
+
+### GIVE SERVICE ACCOUNT ACCESS AS PUBLISHER
+resource "google_pubsub_topic_iam_member" "example-sa-publisher" {
+  project   = var.DEPLOY_PROJECT
+  topic     = google_pubsub_topic.example.name
+  role      = "roles/pubsub.publisher"
+  member    = "serviceAccount:${google_service_account.example.email}"
+}
